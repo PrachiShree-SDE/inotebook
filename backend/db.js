8ffbcee4f6dac1d390d287921f4ff6d77b6220c1
PrachiShree-SDE <prachishree111@gmail.com>
2026-03-10 22:25:53 +0530

    backend: add models, routes and improve user POST

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb:url"
+const mongoURI = "mongodb://localhost:27017/inotebook"
 
 // const connectToMongo = async() => {
 //    mongoose.connect(mongoURI, () => {
